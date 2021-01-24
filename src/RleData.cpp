1baed8de5d13bb4297b9d90a0b07fdb909077d75
Sydney Nguyen <sydneykn@usc.edu>
2021-01-23 17:51:44 -0800

    part 1

--- src/RleData.cpp
+++ src/RleData.cpp
@@ -1,13 +1,114 @@
 #include "RleData.h"
+#include <iostream>
+#include <vector>
 
 void RleData::Compress(const char* input, size_t inSize)
 {
-	// TODO
+    // Clean up any prior data & allocate mData
+    delete [] mData;
+    mSize = 0;
+    mData = new char[inSize*2];
+    
+    // Single char edge case
+    if(inSize == 1){
+        mData[mSize++] = 1;
+        mData[mSize++] = input[0];
+        return;
+    }
+    
+    // Temp storage vector
+    std::vector<char> temp;
+    int tempSize = 0;
+    
+    // First pass -- fill vector
+    char prevChar = NULL;
+    int counter = 0;
+    for(int i = 0; i < inSize; i++){
+        char currChar = input[i];
+        if(currChar == prevChar){
+            counter++;
+            if((i == inSize-1) || (counter == 127)){
+                temp.push_back(counter);
+                tempSize++;
+                temp.push_back(currChar);
+                tempSize++;
+                counter = 0;
+            }
+        }
+        else if(prevChar){
+            temp.push_back(counter);
+            tempSize++;
+            temp.push_back(prevChar);
+            tempSize++;
+            prevChar = currChar;
+            counter = 1;
+            if(i == inSize-1){ //last iteration
+                temp.push_back(counter);
+                temp.push_back(currChar);
+            }
+        }
+        else{ // first iteration
+            prevChar = currChar;
+            counter++;
+        }
+    }
+    
+    int negStart = -1;
+    int negEnd = 0;
+    for(int i = 0; i < temp.size(); i+=2){
+        
+        if((temp[i] != 1) && (negStart == -1)){ // no neg run IP & positive run
+            mData[mSize++] = temp[i];
+            mData[mSize++] = temp[i+1];
+            continue;
+        }
+        else if(negStart == -1){ // start of neg run
+            negStart = i;
+            continue;
+        }
+        else if(temp[i] == 1){ // continuing neg run
+            negEnd = i;
+        }
+        if((i == temp.size()-2) || (temp[i] != 1) || (((negEnd-negStart)+2) /2 == 127)){ //end of neg run or last run
+            int numNeg = ((negEnd-negStart) + 2) / 2;
+            mData[mSize++] = numNeg*-1;
+            for(int j = negStart+1; j < negStart+(negEnd-negStart)+2; j+=2){
+                mData[mSize++] = temp[j];
+            }
+            if(temp[i] != 1){
+                mData[mSize++] = temp[i];
+                mData[mSize++] = temp[i+1];
+            }
+            negStart = -1;
+        }
+    }
+    
 }
 
 void RleData::Decompress(const char* input, size_t inSize, size_t outSize)
 {
-	// TODO
+    // Clean up any prior data & allocate mData
+    delete [] mData;
+    mSize = 0;
+    mData = new char[outSize];
+
+    
+
+    for(int i = 0; i < inSize; i+=2){
+        if(input[i] > 0){ //positive run
+            for(int j = 0; j < input[i]; j++){
+                mData[mSize++] = input[i+1];
+            }
+        }
+       else if(input[i] < 0){ //negative run
+            for(int j = i+1; j <= i+input[i]*-1; j++){
+                mData[mSize++] = input[j];
+            }
+            i += (input[i]*-1) -1;
+
+        }
+    }
+    
 }
 
 std::ostream& operator<< (std::ostream& stream, const RleData& rhs)
--- tests/StudentTests.cpp
+++ tests/StudentTests.cpp
@@ -31,94 +31,90 @@ TEST_CASE("RLE Compression", "[student]")
 		REQUIRE(result);
 	}
     
-    SECTION("Long positive run")
+    SECTION("A very long positive run larger than max size")
     {
         char test[] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-        char expected[] = "\x50" "a";
-        
+        char expected[] = "\x7F" "a" "\x21" "a";
+
         bool result = RunCompressionTest(test, sizeof(test) - 1, expected, sizeof(expected) - 1);
-        
+
         REQUIRE(result);
     }
-    
-    SECTION("Long negative run")
+
+    SECTION("Basic negative run")
     {
-        char test[] = "alalalalalalalalalalalalalalalalalalalal"
-        "alalalalalalalalalalalalalalalalalalalal";
-        char expected[] = "\xB0" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l"
-        "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l"
-        "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l";
-        
+        char test[] = "aaaaaabcde";
+        char expected[] = "\x6" "a" "\xFC" "b" "c" "d" "e";
+
         bool result = RunCompressionTest(test, sizeof(test) - 1, expected, sizeof(expected) - 1);
-        
+
         REQUIRE(result);
     }
     
     SECTION("Alternating positive and negative runs")
     {
-        char test[] = "aaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqp"
-        "aaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqp";
-        char expected[] = "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p"
-        "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p"
-        "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p"
-        "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p";
-        
+        char test[] = "aaaaapqpqpaaaaapqpqp";
+        char expected[] = "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p";
+
         bool result = RunCompressionTest(test, sizeof(test) - 1, expected, sizeof(expected) - 1);
-        
+
         REQUIRE(result);
     }
-    
+
     SECTION("Single char")
     {
         char test[] = "a";
         char expected[] = "\x01" "a";
-        
+
         bool result = RunCompressionTest(test, sizeof(test) - 1, expected, sizeof(expected) - 1);
-        
-        REQUIRE(result);
+
     }
     
-    SECTION("Very long positive run that is greater than max size")
+    SECTION("Long negative run")
     {
-        char test[] = "alalalalalalalalalalalalalalalalalalalal"
-        "alalalalalalalalalalalalalalalalalalalal";
-        char expected[] = "\xB0" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l"
-        "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l"
-        "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l";
-        
+        char test[] = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+        char expected[] = "\x81" "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw" "\xFD" "xyz";
+
         bool result = RunCompressionTest(test, sizeof(test) - 1, expected, sizeof(expected) - 1);
-        
+
         REQUIRE(result);
     }
-    
-    SECTION("")
+
+    SECTION("Long positive run")
     {
-        char test[] = "alalalalalalalalalalalalalalalalalalalal"
-        "alalalalalalalalalalalalalalalalalalalal";
-        char expected[] = "\xB0" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l"
-        "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l"
-        "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l";
-        
+        char test[] = "aaaaaaaaaabbbbbbbbbbccccccccccddddddddddaaaaaaaaaabbbbbbbbbbccccccccccddddddddddaaaaaaaaaabbbbbbbbbbccccccccccddddddddddaaaaaaaaaabbbbbbbbbbccccccccccddddddddddaaaaaaaaaabbbbbbbbbbccccccccccddddddddddaaaaaaaaaabbbbbbbbbbccccccccccdddddddddd";
+        char expected[] = "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d" "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d" "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d" "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d" "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d" "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d";
+
         bool result = RunCompressionTest(test, sizeof(test) - 1, expected, sizeof(expected) - 1);
-        
+
         REQUIRE(result);
     }
     
-    SECTION("")
+    SECTION("Long alternating positive and negative runs")
     {
-        char test[] = "alalalalalalalalalalalalalalalalalalalal"
-        "alalalalalalalalalalalalalalalalalalalal";
-        char expected[] = "\xB0" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l"
-        "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l"
-        "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l" "a" "l";
-        
+        char test[] = "aaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqp";
+        char expected[] = "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p""\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p";
+
         bool result = RunCompressionTest(test, sizeof(test) - 1, expected, sizeof(expected) - 1);
-        
+
+        REQUIRE(result);
+    }
+
+   
+    SECTION("Short positive run")
+    {
+        char test[] = "aaa";
+        char expected[] = "\x03" "a";
+
+        bool result = RunCompressionTest(test, sizeof(test) - 1, expected, sizeof(expected) - 1);
+
         REQUIRE(result);
     }
     
-	
+
 	// TODO: Add more test case sections!!
 }
 
@@ -128,72 +124,122 @@ TEST_CASE("RLE Decompression", "[student]")
 	{
 		char test[] = "\x28" "x";
 		char expected[] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-		
+
 		bool result = RunDecompressionTest(test, sizeof(test) - 1,
 										   expected, sizeof(expected) - 1);
 		REQUIRE(result);
 	}
-	
+    
+    SECTION("Small negative run")
+    {
+        char test[] = "\xFB" "abcde";
+        char expected[] = "abcde";
+
+        bool result = RunDecompressionTest(test, sizeof(test) - 1,
+                                           expected, sizeof(expected) - 1);
+        REQUIRE(result);
+    }
+
+    SECTION("Basic negative run")
+    {
+        char test[] = "\xF6" "abcdeabcde";
+        char expected[] = "abcdeabcde";
+
+        bool result = RunDecompressionTest(test, sizeof(test) - 1,
+                                           expected, sizeof(expected) - 1);
+        REQUIRE(result);
+    }
+
+    SECTION("Long positive run")
+    {
+        char test[] = "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d" "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d" "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d" "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d" "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d" "\xA" "a" "\xA" "b" "\xA" "c" "\xA" "d";
+        char expected[] = "aaaaaaaaaabbbbbbbbbbccccccccccddddddddddaaaaaaaaaabbbbbbbbbbccccccccccddddddddddaaaaaaaaaabbbbbbbbbbccccccccccddddddddddaaaaaaaaaabbbbbbbbbbccccccccccddddddddddaaaaaaaaaabbbbbbbbbbccccccccccddddddddddaaaaaaaaaabbbbbbbbbbccccccccccdddddddddd";
+
+        bool result = RunDecompressionTest(test, sizeof(test) - 1,
+                                           expected, sizeof(expected) - 1);
+        REQUIRE(result);
+    }
+
+    SECTION("Long alternating positive and negative runs")
+    {
+        char test[] = "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p""\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p" "\x05" "a" "\xFB" "p" "q" "p" "q" "p";
+        char expected[] =  "aaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqpaaaaapqpqp";
+
+        bool result = RunDecompressionTest(test, sizeof(test) - 1,
+                                           expected, sizeof(expected) - 1);
+        REQUIRE(result);
+    }
+    
+    SECTION("Long negative run")
+    {
+
+        char test[] = "\x81" "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw" "\xFD" "xyz";
+        char expected[] = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+
+        bool result = RunDecompressionTest(test, sizeof(test) - 1, expected, sizeof(expected) - 1);
+
+        REQUIRE(result);
+    }
 	// TODO: Add more test case sections!!
 }
 
-TEST_CASE("Command line arguments", "[student]")
-{
-	SECTION("instructions.txt.rl1")
-	{
-		const char* argv[] = {
-			"tests/tests",
-			"data/instructions.txt.rl1"
-		};
-		ProcessCommandArgs(2, argv);
-		bool result = CheckFileMD5("data/instructions.txt", "5e042d2e4bf014e714dd7cfc4dc25aab");
-		REQUIRE(result);
-	}
-}
-
-TEST_CASE("File compression", "[student]")
-{
-	RleFile r;
-	SECTION("rle.bmp")
-	{
-		r.CreateArchive("data/rle.bmp");
-		bool result = CheckFileMD5("data/rle.bmp.rl1", "f2a9d8425d53c664e45d9eb1b53137b9");
-		REQUIRE(result);
-	}
-	SECTION("pic.jpg")
-	{
-		r.CreateArchive("data/pic.jpg");
-		bool result = CheckFileMD5("data/pic.jpg.rl1", "0bbf2a5109b30d79939d2061ea8c74aa");
-		REQUIRE(result);
-	}
-	SECTION("Conquest.ogg")
-	{
-		r.CreateArchive("data/Conquest.ogg");
-		bool result = CheckFileMD5("data/Conquest.ogg.rl1", "ec29ff368ec5100bfba22635ddc5ba5c");
-		REQUIRE(result);
-	}
-}
-
-TEST_CASE("File decompression", "[student]")
-{
-	RleFile r;
-	SECTION("works.bmp.rl1")
-	{
-		r.ExtractArchive("data/works.bmp.rl1");
-		bool result = CheckFileMD5("data/works.bmp", "8baad647acefae2f8c36ee111582a875");
-		REQUIRE(result);
-	}
-	SECTION("xkcd.bmp.rl1")
-	{
-		r.ExtractArchive("data/xkcd.bmp.rl1");
-		bool result = CheckFileMD5("data/xkcd.bmp", "a4d7efa89d47a94a76f8050dd8d60cd0");
-		REQUIRE(result);
-	}
-	SECTION("logo.png.rl1")
-	{
-		r.ExtractArchive("data/logo.png.rl1");
-		bool result = CheckFileMD5("data/logo.png", "95403309460632f60efa647ef59b78fc");
-		REQUIRE(result);
-	}
-}
-
+//TEST_CASE("Command line arguments", "[student]")
+//{
+//	SECTION("instructions.txt.rl1")
+//	{
+//		const char* argv[] = {
+//			"tests/tests",
+//			"data/instructions.txt.rl1"
+//		};
+//		ProcessCommandArgs(2, argv);
+//		bool result = CheckFileMD5("data/instructions.txt", "5e042d2e4bf014e714dd7cfc4dc25aab");
+//		REQUIRE(result);
+//	}
+//}
+//
+//TEST_CASE("File compression", "[student]")
+//{
+//	RleFile r;
+//	SECTION("rle.bmp")
+//	{
+//		r.CreateArchive("data/rle.bmp");
+//		bool result = CheckFileMD5("data/rle.bmp.rl1", "f2a9d8425d53c664e45d9eb1b53137b9");
+//		REQUIRE(result);
+//	}
+//	SECTION("pic.jpg")
+//	{
+//		r.CreateArchive("data/pic.jpg");
+//		bool result = CheckFileMD5("data/pic.jpg.rl1", "0bbf2a5109b30d79939d2061ea8c74aa");
+//		REQUIRE(result);
+//	}
+//	SECTION("Conquest.ogg")
+//	{
+//		r.CreateArchive("data/Conquest.ogg");
+//		bool result = CheckFileMD5("data/Conquest.ogg.rl1", "ec29ff368ec5100bfba22635ddc5ba5c");
+//		REQUIRE(result);
+//	}
+//}
+//
+//TEST_CASE("File decompression", "[student]")
+//{
+//	RleFile r;
+//	SECTION("works.bmp.rl1")
+//	{
+//		r.ExtractArchive("data/works.bmp.rl1");
+//		bool result = CheckFileMD5("data/works.bmp", "8baad647acefae2f8c36ee111582a875");
+//		REQUIRE(result);
+//	}
+//	SECTION("xkcd.bmp.rl1")
+//	{
+//		r.ExtractArchive("data/xkcd.bmp.rl1");
+//		bool result = CheckFileMD5("data/xkcd.bmp", "a4d7efa89d47a94a76f8050dd8d60cd0");
+//		REQUIRE(result);
+//	}
+//	SECTION("logo.png.rl1")
+//	{
+//		r.ExtractArchive("data/logo.png.rl1");
+//		bool result = CheckFileMD5("data/logo.png", "95403309460632f60efa647ef59b78fc");
+//		REQUIRE(result);
+//	}
+//}
+//
